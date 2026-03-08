cc64dc9987c10db8c3c4ec4e7c339143c627071a
wes2000 <whannasch@gmail.com>
2026-03-07 21:45:43 -0600

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,14 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist'
+  },
+  server: {
+    proxy: {
+      '/nhl-api': {
+        target: 'https://api-web.nhle.com/v1',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/nhl-api/, '')
+      }
+    }
   }
 })
